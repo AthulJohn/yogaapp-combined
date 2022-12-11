64c927df61cd3b7fd8ef5874f0beb370a85a69f1
AthulJohn <athulpulickaljohn@gmail.com>
2022-12-11 19:43:48 +0530

    Added CORS headers

--- app.py
+++ app.py
@@ -47,6 +47,14 @@ app.register_blueprint(register_blueprint)
 
 #routes Folder contains various route functions
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  response.headers.add('Access-Control-Allow-Credentials', 'true')
+  return response
+
 if __name__ == '__main__':
     app.run()
 
